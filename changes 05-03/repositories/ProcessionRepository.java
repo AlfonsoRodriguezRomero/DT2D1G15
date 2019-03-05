ec4b34998ff576cdb36db89bdae8ab79c4c61664
Alfonso Rodriguez <2aalfonsoromero@gmail.com>
2019-03-05 21:10:47 +0100

    update for queries in ProcessionRepository
    processionsIn30days query updated in ProcessionRepository

---
+++ changes 05-03/repositories/ProcessionRepository.java
@@ -0,0 +1,29 @@
+
+package repositories;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import domain.Procession;
+
+@Repository
+public interface ProcessionRepository extends JpaRepository<Procession, Integer> {
+
+	@Query("select p from Procession p where p.brotherhood.id = ?1")
+	Collection<Procession> findByBrotherhoodId(int id);
+
+	//Show to other actors only processions in final mode
+	@Query("select p from Procession p where p.finalMode='true'")
+	Collection<Procession> findAllInFinalMode();
+
+	//12.3
+
+	@Query("select p from Procession p where datediff(p.moment,current_timestamp)<=30")
+	Collection<Procession> processionsIn30days();
+
+	//select p from Procession p where p.moment between current_timestamp() and dateadd(day,30,current_timestamp())
+	//select p from Procession p where p.moment > current_timestamp()
+}
